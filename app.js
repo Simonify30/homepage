334ef3d80ca550a17fc948bc839d6d090f8468d3
ifeanyi simon <simonify30@yahoo.com>
2023-03-14 09:57:16 +0100

    my commit - committing all files to the repository

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const menu = document.querySelector('.menu');
 const remove = document.querySelector('.remove');
 const dropdown = document.querySelector('.dropdown')
+const body = document.querySelector('body');
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
 
-menu.addEventListener('click', function(){
-    dropdown.style.visibility = 'visible'
-})
+
+const toggleBackdrop = () =>{
+    backdrop.classList.toggle('visible');
+};
+
+const toggleMenu = () =>{
+    dropdown.classList.toggle('visible');
+    toggleBackdrop();
+};
+
+const backdropClick = () =>{
+    toggleMenu();
+};
 
 
-remove.addEventListener('click', function(){
-    dropdown.style.visibility = 'hidden'
-
-})
+menu.addEventListener('click', toggleMenu);
+remove.addEventListener('click', toggleMenu);
+backdrop.addEventListener('click', toggleMenu);
